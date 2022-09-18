f6d4b71028d2a2df126df2a9b503f680db87da26
Bruno <104589217+brunott98@users.noreply.github.com>
2022-09-18 19:27:27 -0300

    Update datas.java

--- datas.java
+++ datas.java
@@ -2,7 +2,7 @@ package Estudos;
 
 import java.util.Date;
 
-public class datas {
+public class Datas {
     public static void main(String[] args){
         Date dia = new Date();
         System.out.println(dia);
