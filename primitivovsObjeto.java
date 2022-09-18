a785bfbef74f82af543ea6b4790d321fe19565f6
Bruno <104589217+brunott98@users.noreply.github.com>
2022-09-17 22:27:49 -0300

    Add files via upload

---
+++ primitivovsObjeto.java
@@ -0,0 +1,16 @@
+package Estudos;
+
+public class primitivovsObjeto {
+
+    //Wrappers são tipos primitivos que se tornaram objetos
+
+    public static void main(String[] args) {
+        Byte b = 100;  //cria um wrapper do primitivo byte
+        Short k = 1000;
+        Integer i = 10000;
+        Long z = 1000000L;
+
+
+    }
+
+}
