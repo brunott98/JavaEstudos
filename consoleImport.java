25eef31451f2146a74bc2bc3cd0c10f812d7419e
Bruno <104589217+brunott98@users.noreply.github.com>
2022-09-17 22:23:40 -0300

    Add files via upload

---
+++ consoleImport.java
@@ -0,0 +1,29 @@
+package Estudos;
+
+import java.util.Scanner;
+
+public class consoleImport {
+    public static void main(String[] args) {
+
+        System.out.printf("Salário: %.1f%n",12000.20);
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: "); //print não pula linha
+        String nome = entrada.nextLine();
+
+        System.out.print("Informe seu hobbie: "); //print não pula linha
+        String hobbie = entrada.nextLine();
+
+        System.out.print("Informe sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.printf("O hobbie de %s é %s e possui %s anos.%n",nome,hobbie,idade);
+
+
+
+        entrada.close();
+
+
+    }
+}
